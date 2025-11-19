8b5dee23ad7b50416959ab9687e8025ee469ec27
Sand-form <73169688+Sand-form@users.noreply.github.com>
2025-11-19 16:27:29 +0800

    init commit

---
+++ src/test/java/org/depress/neuroscanm/NeuroScanMApplicationTests.java
@@ -0,0 +1,13 @@
+package org.depress.neuroscanm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroScanMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
